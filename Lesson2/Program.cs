08488d66d71077d93c7218e01080d73916393095
Paul de Mast <pjf.demast@avans.nl>
2021-02-09 13:12:54 +0100

    Added: - table Teams - one to many between team and Member

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -11,37 +11,48 @@ namespace Lesson2 {
             Console.WriteLine("Team management");
             using var teamsManagementContext = new TeamManagmentDBContext();
 
-            teamsManagementContext.Members.Add(new Member { Name = "Jeroen", Email = "jeroen@breda.nl" });
+            var team2 = teamsManagementContext.Teams
+                .Include(team => team.Enrolled)
+                .FirstOrDefault(team => team.Group == "Groep-2");
 
-            var memberList = new List<Member> {
-                new Member { Name="Henk", Email = "Henk@breda.nl"},
-                new Member { Name="Loes", Email = "Loes@avans.nl"},
-            };
-
-            teamsManagementContext.Members.AddRange(memberList);
-            teamsManagementContext.SaveChanges();
-
-            var selectedStudents = teamsManagementContext
-                .Members
-                .Where(member => member.Name.Contains("oe"))
-                .OrderBy(member => member.Name)
-                .ToList();
-
-            foreach (var member in selectedStudents) {
+            Console.WriteLine(team2.Title);
+            foreach (var member in team2.Enrolled) {
                 Console.WriteLine(member.Name);
             }
 
-            Console.WriteLine("before update");
-            Member before = teamsManagementContext.Members.FirstOrDefault(Member => Member.Name == "Henk");
-            Console.WriteLine(before.Email);
 
-            before.Email = "henk@gmail.nl";
-            teamsManagementContext.SaveChanges();
+            #region
+            //teamsManagementContext.Members.Add(new Member { Name = "Jeroen", Email = "jeroen@breda.nl" });
 
-            Console.WriteLine("after update");
-            Member after = teamsManagementContext.Members.FirstOrDefault(Member => Member.Name == "Henk");
-            Console.WriteLine(after.Email);
+            //var memberList = new List<Member> {
+            //    new Member { Name="Henk", Email = "Henk@breda.nl"},
+            //    new Member { Name="Loes", Email = "Loes@avans.nl"},
+            //};
 
+            //teamsManagementContext.Members.AddRange(memberList);
+            //teamsManagementContext.SaveChanges();
+
+            //var selectedStudents = teamsManagementContext
+            //    .Members
+            //    .Where(member => member.Name.Contains("oe"))
+            //    .OrderBy(member => member.Name)
+            //    .ToList();
+
+            //foreach (var member in selectedStudents) {
+            //    Console.WriteLine(member.Name);
+            //}
+
+            //Console.WriteLine("before update");
+            //Member before = teamsManagementContext.Members.FirstOrDefault(Member => Member.Name == "Henk");
+            //Console.WriteLine(before.Email);
+
+            //before.Email = "henk@gmail.nl";
+            //teamsManagementContext.SaveChanges();
+
+            //Console.WriteLine("after update");
+            //Member after = teamsManagementContext.Members.FirstOrDefault(Member => Member.Name == "Henk");
+            //Console.WriteLine(after.Email);
+            #endregion
         }
     }
 }
