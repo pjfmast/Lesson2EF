4f1453c13803759ac62678b69ef162fbb9e34b41
Paul de Mast <pjf.demast@avans.nl>
2021-02-09 13:59:24 +0100

    Remove Repositories, focus demo on EF

--- Lesson2/Models/ITeamRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lesson2.Models {
-    public interface ITeamRepository {
-        ICollection<Team> AllTeams { get; }
-    }
-}
