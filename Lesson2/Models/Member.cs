dbd20176bc1187745cd86d30c6aa41e1d12ae039
Paul de Mast <pjf.demast@avans.nl>
2021-02-09 17:45:33 +0100

    Remove commented code

--- Lesson2/Models/Member.cs
+++ Lesson2/Models/Member.cs
@@ -6,8 +6,5 @@ namespace Lesson2.Models {
         public string Name { get; set; }
         public string Email { get; set; }
 
-        //public Team Team { get; set; }
-        //public int TeamId { get; set; }
-
     }
 }
