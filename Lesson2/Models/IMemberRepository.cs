576df73dd342b8af4cf28184ff716b234bf6370e
Paul de Mast <pjf.demast@avans.nl>
2021-02-09 13:57:47 +0100

    Remove Repositories, focus demo on EF

--- Lesson2/Models/IMemberRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lesson2.Models {
-    interface IMemberRepository {
-        ICollection<Member> AllMembers();
-    }
-}
